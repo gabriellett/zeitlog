a76fe707f698952dbc53e6e1ed423a538ae56943
Gabriel Lett Viviani <gm.lett@gmail.com>
2014-10-18 21:21:23 -0300

    Basic imports and configuration of scrollreveal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require bootstrap
+//= require scrollReveal.js/scrollReveal.js
 //= require_tree ./pages
 //= require zeitlog
---
+++ app/assets/javascripts/zeitlog.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+
+  window.sr= new scrollReveal({
+    reset: true,
+    move: '50px',
+    mobile: true
+  });
+
+});
