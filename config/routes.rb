3b06aaa5748b7b92013c8cd30d5d9bb9f610eca0
Gabriel Lett Viviani <gm.lett@gmail.com>
2014-10-19 17:55:45 -0200

    Routes to new controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  resources :time_logs
+
+  resources :projects
+
   devise_for :users, controllers: {
     :omniauth_callbacks => "users/omniauth_callbacks"
   }
