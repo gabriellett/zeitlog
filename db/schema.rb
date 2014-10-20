a6069e95055b835ffa4c27a171d7616693b25692
Gabriel Lett Viviani <gm.lett@gmail.com>
2014-10-19 23:18:32 -0200

    Updating schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141015234300) do
+ActiveRecord::Schema.define(version: 20141019195129) do
+
+  create_table "projects", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "time_logs", force: true do |t|
+    t.integer  "project_id"
+    t.integer  "user_id"
+    t.integer  "hours"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "time_logs", ["project_id"], name: "index_time_logs_on_project_id"
+  add_index "time_logs", ["user_id"], name: "index_time_logs_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
